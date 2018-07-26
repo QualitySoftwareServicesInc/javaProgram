3cef4a933b932e7afd6e103fe083f2312bf6ea62
SHANTH2013 <shantha_mh@qssinc.com>
2018-07-26 06:22:43 -0400

    Update Application.java
    New code added

--- Application.java
+++ Application.java
@@ -1,7 +1,23 @@
-public class Application {
- 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Hello {
+    public static void main(String[]args){
+
+        System.out.println("Hello Shanthamurthy!");
+
+        int myFirstNumber = (10 + 5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myNewTotal = 1000 - myTotal;
+
+        System.out.println(myNewTotal);
+
     }
- 
 }
+
+    /**How would you define a new variable
+     * that had a difference
+     * between 1,000 and the current value
+     * of 'myTotal'
+     */
